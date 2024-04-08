8a8d54c6d88700360c26c6480f186e9a205471ff
moumita-ghose <163099608+moumita-ghose@users.noreply.github.com>
2024-04-08 12:36:44 +0600

    Create Assiut University Training - Newcomers(SHEET-1)(A).c

---
+++ Assiut University Training - Newcomers(SHEET-1)(A).c
@@ -0,0 +1,18 @@
+/*
+author : MOumITA GHOSE
+problem link :https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
+
+*/
+//         code  starts from here:
+//------------------------------------------
+
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    gets(str);
+    fflush(stdin);
+    printf("Hello, %s\n",str);
+ 
+    return 0;
+}

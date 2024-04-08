b8fba0fa4c51d4dda50bc362000f42c2110c18eb
moumita-ghose <163099608+moumita-ghose@users.noreply.github.com>
2024-04-08 13:08:34 +0600

    Create Create Create Assiut University Training - Newcomers(SHEET-1)(E).c

---
+++ Create Create Assiut University Training - Newcomers(SHEET-1)(E).c
@@ -0,0 +1,16 @@
+/*
+  AUTHOR : MOUMITA GHOSE
+  problem link :https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+   DATE : 08/04/2024
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    double r,pi=3.141592653,area;
+    scanf("%lf",&r);
+    area=pi*r*r;
+    printf("%.9lf\n",area);
+    return 0;
+}

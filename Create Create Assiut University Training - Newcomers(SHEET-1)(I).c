9855737c6d8da49f85685b668b52479abde2f2fa
moumita-ghose <163099608+moumita-ghose@users.noreply.github.com>
2024-04-08 14:06:23 +0600

    Create 	 Create Create Assiut University Training - Newcomers(SHEET-1)(I).c

---
+++ Create Create Assiut University Training - Newcomers(SHEET-1)(I).c
@@ -0,0 +1,18 @@
+/*
+AUTHOR : MOUMITA GHOSE
+PROBLEM LINK : https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+DATE : 08/04/2024
+*/
+#include <stdio.h>
+ 
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>=b){
+        printf("Yes\n");
+    }else{
+        printf("No\n");
+    }
+    return 0;
+}
